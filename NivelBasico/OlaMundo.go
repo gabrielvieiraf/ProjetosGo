4563092618fb223531f2d06097211f6b56fcb023
Gabriel Vieira <48156370+gabrielvieiraf@users.noreply.github.com>
2020-05-19 16:43:00 -0300

    Comentários Adicionados

--- NivelBasico/OlaMundo.go
+++ NivelBasico/OlaMundo.go
@@ -1,6 +1,7 @@
-package main
-import "fmt"
-
+//Um Package (Pacote) é uma unidade composta por arquivos fonte que juntos servem a um propósito comum. 
+package main //Pacote Executável
+import "fmt" //fmt é um pacote que pode ser reutilizado dentro de outros pacotes, também conhecido como biblioteca.
+//Função main onde darei um 'oi' através da função Println presente no pacote 'fmt'.
 func main(){
-  fmt.Println("Ola Mundo!")
+  fmt.Println("Ola Mundo!") 
 }
