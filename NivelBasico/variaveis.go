6781ecf9232938f49458abdb412d5f5f86622da6
Gabriel Vieira <48156370+gabrielvieiraf@users.noreply.github.com>
2020-05-29 11:50:57 -0300

    Create variaveis.go

---
+++ NivelBasico/variaveis.go
@@ -0,0 +1,44 @@
+//Exemplo 2: Tipos de variável
+//Ao escolher um tipo para nossas variáveis, devemos nos atentar
+//em suas características e também à quantidade de memória necessária para armazená-la.
+package main
+
+import (
+    "fmt"
+    "strings"
+)
+
+func main() {
+       
+        //Declaração de variáveis e inicializando
+        
+        //Inteiros
+        var a int = 3    //Variável inteira   - Tamanho: 4 bytes/ 32 bits
+        //Varias formas de inteiro: int8 int16 int32 int64 uint uint8 uint16 uint32 uint64 uintptr
+        var b byte       //Variável byte      - Tamanho: 1 byte / 8  bits -Um alias para uint8
+        var c rune       //É um alias int32 e é usada para armazenar Unicode
+        //Ponto Flutuante
+        var d float32 = 2.14   //Variável flutuante - Tamanho: 4 bytes/ 32 bits ou 64 bits
+        
+        var nome string = "Gabriel"
+        
+        boolean f; //Variável booleana (Variável lógica (verdadeiro/falso)))
+               
+       //Inicializando Variáveis
+        
+       fmt.Println("Resultado da soma: ")
+       fmt.Println(a+d)
+        
+       //A precisão que o ponto flutuante dá para a equação
+       //é desnecessária no exemplo da soma
+      
+       //Agora veja a diferença entre os tipos em uma divisão não exata  
+       //Ponto Flutuante
+       System.out.println("Resultado da divisao: ")
+       System.out.println(a/d)
+       variavel := a/2
+       System.out.println("Resultado da divisao:\n ")
+       System.out.println(variavel)
+       //No caso do inteiro, todas as casas após a vírgula são desconsideradas.
+    }   
+}
